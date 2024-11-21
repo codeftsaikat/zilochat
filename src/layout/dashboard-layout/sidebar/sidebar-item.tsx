e5bbd3f7ec3c77861bf67380774339b42280d05d
codeftsaikat <soikatdey18@gmail.com>
2024-11-21 01:26:20 -0800

    update dashboard page with astonishing design

--- src/layout/dashboard-layout/sidebar/sidebar-item.tsx
+++ src/layout/dashboard-layout/sidebar/sidebar-item.tsx
@@ -10,21 +10,28 @@ const SidebarItem = (props: Props) => {
   const location = useLocation();
   const pathname = location.pathname;
   const isDropdown = childs && childs.length > 0;
-  const [isOpen,setOpen] = useState(false)
-  const handleSideNav = ()=>{
-      setOpen(!isOpen);
-  }
+  const [isOpen, setOpen] = useState(false);
+  const handleSideNav = () => {
+    setOpen(!isOpen);
+  };
   return (
-    <li  onClick={handleSideNav}  className={`nav-side-item ${isDropdown ? "sidedropdown" : ""}`}>
+    <li
+      onClick={handleSideNav}
+      className={`nav-side-item ${isDropdown ? "sidedropdown" : ""}`}
+    >
       <Link
         to={href}
         className={`nav-link ${pathname == href ? "active" : ""}`}
       >
         <span className={icon} />
         <span className="link-text">{label}</span>
-        {isDropdown && <span className="arrow"></span>}
+        {isDropdown && (
+          <span
+            className={`arrow ${isOpen ? "d-inline-block fa-rotate-180" : ""}`}
+          ></span>
+        )}
       </Link>
-      {isDropdown &&isOpen && (
+      {isDropdown && isOpen && (
         <ul className="subnav">
           {childs.map((item, idx) => (
             <SidebarSubItem key={idx} {...item} />
